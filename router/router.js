c2470d1ac233311686c679b794551f0de4fae0a7
Jeferson sena <jefinho368@gmail.com>
2024-01-15 11:11:26 -0300

    Configuration cors in router.js

---
+++ router/router.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const categoria = require("../controller/CategoriaController.js");
+const financa = require('../controller/FinancaController.js');
+
+// padrão Rest
+// categoria
+router.post('/criar/categoria', categoria.create);
+router.get('/listar/categoria/:page', categoria.findAll);
+router.put('/atualizar/categoria/:id', categoria.update);
+router.delete('/deletar/categoria/:id', categoria.delete)
+
+// Finanças
+router.post('/criar/financa', financa.create);
+router.get('/listar/financa/:page', financa.findAll);
+
+router.get('/listar/financa/dataInicial/:dataInicial/dataFinal/:dataFinal/page/:page', financa.findAllDate);
+
+router.put('/atualizar/financa/:id', financa.update);
+router.delete('/deletar/financa/:id', financa.delete)
+
+module.exports = router;
